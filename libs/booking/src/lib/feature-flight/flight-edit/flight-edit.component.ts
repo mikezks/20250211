68df7605b04dd127a6271a4d98f2fc431d2b797f
mikezks <23002661+mikezks@users.noreply.github.com>
2025-02-12 15:37:14 +0100

    fix: tidy-up

--- libs/booking/src/lib/feature-flight/flight-edit/flight-edit.component.ts
+++ libs/booking/src/lib/feature-flight/flight-edit/flight-edit.component.ts
@@ -1,11 +1,11 @@
-import { Component, DestroyRef, effect, inject, Injector, input, numberAttribute, runInInjectionContext } from '@angular/core';
-import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { Component, effect, inject, input, numberAttribute } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { routerFeature } from '@flight-demo/shared-state';
 import { Store } from '@ngrx/store';
-import { initialFlight } from '../../logic-flight';
+import { switchMap } from 'rxjs';
 import { FlightService } from '../../api-boarding';
-import { interval, switchMap } from 'rxjs';
+import { initialFlight } from '../../logic-flight';
 
 
 @Component({
@@ -18,8 +18,6 @@ import { interval, switchMap } from 'rxjs';
 export class FlightEditComponent {
   private store = inject(Store);
   private flightService = inject(FlightService);
-  /* private destroyRef = inject(DestroyRef);
-  private injector = inject(Injector); */
 
   readonly id = input(0, { transform: numberAttribute });
   readonly flight = toSignal(
@@ -27,10 +25,6 @@ export class FlightEditComponent {
       switchMap(id => this.flightService.findById(id))
     ), { initialValue: initialFlight }
   );
-
-  /* readonly counterSubscription = interval(1_000).pipe(
-    takeUntilDestroyed()
-  ).subscribe(console.log); */
 
   protected editForm = inject(NonNullableFormBuilder).group({
     id: [0],
@@ -48,17 +42,9 @@ export class FlightEditComponent {
     effect(() => this.editForm.patchValue(
       this.flight()
     ));
-
-    // this.destroyRef.onDestroy(() => console.log('Bye, bye!'))
   }
 
   protected save(): void {
     console.log(this.editForm.value);
-
-    /* runInInjectionContext(this.injector, () => {
-      interval(1_000).pipe(
-        takeUntilDestroyed()
-      ).subscribe(console.log);
-    }); */
   }
 }
