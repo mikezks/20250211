6ad3e6cc29465e6d19ad034550721f8f01429041
mikezks <23002661+mikezks@users.noreply.github.com>
2025-02-12 18:26:47 +0100

    docs: sheriff tags log

--- sheriff.config.ts
+++ sheriff.config.ts
@@ -67,3 +67,35 @@ export const config: SheriffConfig = {
     // add your dependency rules here
   },
 };
+
+
+/**
+ * npx sheriff list .\apps\shell\src\main.ts
+ *
+ * This project contains 23 modules:
+ *
+ * . (root)
+ * ├── apps\shell (domain:shell, type:app)
+ * ├── apps\shell-e2e (domain:shell-e2e, type:app)
+ * ├── libs\domain\boarding\src\lib\feature-departure (domain:boarding, type:feature)
+ * ├── libs\domain\boarding\src\lib\route-boarding (domain:boarding, type:route)
+ * ├── libs\domain\booking\src\lib\api-boarding (domain:booking-api-boarding, type:api)
+ * ├── libs\domain\booking\src\lib\feature-flight (domain:booking, type:feature)
+ * ├── libs\domain\booking\src\lib\logic-flight (domain:booking, type:logic)
+ * ├── libs\domain\booking\src\lib\route-booking (domain:booking, type:route)
+ * ├── libs\domain\booking\src\lib\ui-flight (domain:booking, type:ui)
+ * ├── libs\domain\checkin\src\lib\feature-passenger (domain:checkin, type:feature)
+ * ├── libs\domain\checkin\src\lib\logic-passenger (domain:checkin, type:logic)
+ * ├── libs\domain\checkin\src\lib\route-checkin (domain:checkin, type:route)
+ * ├── libs\shared\core\src\lib\feature-core (domain:shared, type:feature)
+ * ├── libs\shared\core\src\lib\ui-core (domain:shared, type:ui)
+ * ├── libs\shared\logger\src\lib\logic-cd-visualizer (domain:shared, type:logic)
+ * ├── libs\shared\state\src\lib\logic-router-feature (domain:shared, type:logic)
+ * ├── libs\domain\boarding\src (domain:boarding, type:public)
+ * ├── libs\domain\booking\src (domain:booking, type:public)
+ * ├── libs\domain\checkin\src (domain:checkin, type:public)
+ * ├── libs\domain\checkin\src\lib\util-validation (domain:checkin, type:util)
+ * ├── libs\shared\core\src (domain:shared, type:public)
+ * ├── libs\shared\logger\src (domain:shared, type:public)
+ * └── libs\shared\state\src (domain:shared, type:public)
+ */
