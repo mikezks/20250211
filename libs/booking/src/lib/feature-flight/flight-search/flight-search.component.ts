f77eb56dc5984b9be6416c18d30d4a3c9eaedea8
mikezks <23002661+mikezks@users.noreply.github.com>
2025-02-12 13:26:39 +0100

    demo: reactive graph

--- libs/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -3,6 +3,7 @@ import { Component, computed, effect, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
+import { SIGNAL } from '@angular/core/primitives/signals';
 
 
 @Component({
@@ -34,6 +35,8 @@ export class FlightSearchComponent {
 
   constructor() {
     effect(() => console.log(this.route()));
+
+    // console.log(this.route[SIGNAL]);
   }
 
   protected search(filter: FlightFilter): void {
